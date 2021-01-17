8e0dec2d4390493369e2ebeb4f188548193f9552
Angelo <ampsf@icomp.ufam.edu.br>
2021-01-17 08:41:05 -0400

    chore: change color, change images in home

--- src/App.js
+++ src/App.js
@@ -6,59 +6,13 @@ import {
   StyleSheet,
   TouchableOpacity
 } from 'react-native'
+import Home from './pages/Home'
 
 function App() {
-  function teste() {
-    console.warn('oi')
-  }
+ 
   return (
-    <View style={styles.viewMain}>
-
-      <Text style={styles.mainTitle}>7{"\n"}Passos{"\n"}Para a {"\n"}Vida{"\n"}Eterna</Text>
-      <Image
-        style={styles.cruz}
-        source={require('../src/img/cruz.png')}
-      />
-      <TouchableOpacity style={styles.btnStart} onPress={teste}>
-        <Text style={styles.txtStart}>Toque{"\n"}para começar</Text>
-      </TouchableOpacity>
-    </View>)
+ <Home/>
+    )
 }
-const styles = StyleSheet.create({
-  viewMain: {
-    flex: 1,
-    alignItems: 'center',
-    backgroundColor: '#94D1CE',
-
-  },
-  cruz:{
-    width:50,
-    height:50,
-    marginTop:100
-  },
-  mainTitle: {
-    marginTop: 80,
-    fontSize: 40,
-    fontWeight: 'bold',
-    color:'#A94343',
-    textAlign:'center',
-    fontFamily:''
-  },
-  btnStart: {
-    backgroundColor: '#E87D00',
-    borderWidth: 2,
-    padding: 12,
-    borderRadius: 15,
-    marginTop:100,
-    height:'10%',
-    width:'30%'
-  },
-  txtStart: {
-    textAlign: 'center',
-    fontWeight:'bold'
-
-  }
-
-});
 
 export default App
---
+++ src/pages/Home/index.js
@@ -0,0 +1,64 @@
+import React from 'react'
+import {
+  Text,
+  View,
+  Image,
+  StyleSheet,
+  TouchableOpacity
+} from 'react-native'
+
+function Home() {
+  function teste() {
+    console.warn('oi')
+  }
+  return (
+    <View style={styles.viewMain}>
+
+      <Text style={styles.mainTitle}>7{"\n"}Passos{"\n"}Para a {"\n"}Vida{"\n"}Eterna</Text>
+      <Image
+        style={styles.cruz}
+        source={require('../../img/passo.png')}
+      />
+      <TouchableOpacity style={styles.btnStart} onPress={teste}>
+        <Text style={styles.txtStart}>Toque{"\n"}para começar</Text>
+      </TouchableOpacity>
+    </View>)
+}
+const styles = StyleSheet.create({
+  viewMain: {
+    flex: 1,
+    alignItems: 'center',
+    backgroundColor: '#409fff',
+
+  },
+  cruz:{
+    width:50,
+    height:50,
+    marginTop:100
+  },
+  mainTitle: {
+    marginTop: 80,
+    fontSize: 40,
+    fontWeight: 'bold',
+    color:'#FFFFFF',
+    textAlign:'center',
+    fontFamily:''
+  },
+  btnStart: {
+    backgroundColor: '#000',
+    borderWidth: 2,
+    padding: 12,
+    borderRadius: 50,
+    marginTop:100,
+    height:'10%',
+    width:'30%'
+  },
+  txtStart: {
+    textAlign: 'center',
+    fontWeight:'bold',
+    color:'#409fff'
+  }
+
+});
+
+export default Home
